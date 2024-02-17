d768f391b600033e91a65b1bb9c001358a1938ea
Admin <Admin@DESKTOP-OOAIQ5U>
2024-02-17 18:19:11 +0300

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    public class Matrix
+    {
+        public int CountRows;
+        public int CountColumns;
+        public int[,] values;
+
+        public Matrix() { }
+        public Matrix(int CountRows, int CountColumns) //конструктор
+        {
+            this.CountRows = CountRows;
+            this.CountColumns = CountColumns;
+            this.values = new int[CountRows, CountColumns];
+        }
+
+        public void matrixFilling(Matrix matrix)  //заполнение матрицы
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < matrix.CountRows; i++)
+            {
+                for (int j = 0; j < matrix.CountColumns; j++)
+                    matrix.values[i, j] = rnd.Next(1, 10);
+            }
+        }
+
+        public int[,] matrixAddition(Matrix matrix1, Matrix matrix2)   //сложение матриц
+        {
+            Matrix matrix = new Matrix(matrix1.CountRows, matrix1.CountColumns);
+
+            if (matrix1.CountRows != matrix2.CountRows || matrix1.CountColumns != matrix2.CountColumns)
+            {
+                throw new Exception("Разное кол-во строк/столбцов. Невозможно сложить");
+            }
+
+            for (int i = 0; i < matrix1.CountRows; i++)
+            {
+                for (int j = 0; j < matrix1.CountColumns; j++)
+                {
+                    matrix.values[i, j] = matrix1.values[i, j] + matrix2.values[i, j];
+                }
+            }
+            return matrix.values;
+        }
+
+
+        public int[,] matrixSubtraction(Matrix matrix1, Matrix matrix2)  //вычитание матриц
+        {
+            Matrix matrix = new Matrix(matrix1.CountRows, matrix1.CountColumns);
+
+            if (matrix1.CountRows != matrix2.CountRows || matrix1.CountColumns != matrix2.CountColumns)
+            {
+                throw new Exception("Разное кол-во строк/столбцов. Невозможно вычесть");
+            }
+
+            for (int i = 0; i < matrix1.CountRows; i++)
+            {
+                for (int j = 0; j < matrix1.CountColumns; j++)
+                {
+                    matrix.values[i, j] = matrix1.values[i, j] - matrix2.values[i, j];
+                }
+            }
+            return matrix.values;
+        }
+
+
+        public int[,] matrixMultiply(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix matrix = new Matrix(matrix1.CountRows, matrix2.CountColumns);
+
+            if (matrix1.CountColumns != matrix2.CountRows || matrix1.CountRows != matrix2.CountColumns)
+            {
+                throw new Exception("Невозможно выполнить умножение");
+            }
+
+            for (int i = 0; i < matrix1.values.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.values.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matrix2.values.GetLength(0); k++)
+                    {
+                        matrix.values[i, j] += matrix1.values[i, k] * matrix2.values[k, j];
+                    }
+                }
+            }
+            return matrix.values;
+        }
+
+        public void matrixInfo(Matrix matrix)   //вывод информации о матрице
+        {
+            for (int i = 0; i < matrix.CountRows; i++)
+            {
+                for (int j = 0; j < matrix.CountColumns; j++)
+                {
+                    Console.Write(matrix.values[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Headers;
+
+namespace matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Матрица 1: ");
+            int CountRows;
+            int CountColumns;
+
+            rowsColumnsInput(out CountRows, out CountColumns);
+            Matrix matrix1 = new Matrix(CountRows, CountColumns); //конструктор
+
+            Console.WriteLine("Матрица 2: ");
+            rowsColumnsInput(out CountRows, out CountColumns);
+            Matrix matrix2 = new Matrix(CountRows, CountColumns); //конструктор
+
+            matrix1.matrixFilling(matrix1);  //заполнение матрицы
+            matrix1.matrixFilling(matrix2);  //заполнение матрицы
+
+            Console.WriteLine("\nМатрица 1: ");
+            matrix1.matrixInfo(matrix1); //вывод матриц
+
+            Console.WriteLine("\nМатрица 2: ");
+            matrix2.matrixInfo(matrix2);
+
+            Console.WriteLine("\nРезультат сложения: ");
+            int[,] matr = matrix1.matrixAddition(matrix1, matrix2);
+
+
+        }
+
+        static void rowsColumnsInput(out int CountRows, out int CountColumns)
+        {
+            Console.Write("Введите кол-во строк: ");
+            CountRows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите кол-во столбцов: ");
+            CountColumns = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+        }
+    }
+}
+
+
